7a65fe9df4ed9afd9754d6516f851a40a5bf11ea
Venkatesh <venkatsiva2609@gmail.com>
2025-07-28 00:37:03 +0530

    initial files

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: 'pages', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
